147920ff93f33664e40b44b3d2d83c94248e74ef
TuNombreDeUsuario <TuCorreoElectronico>
2024-01-27 16:28:14 -0500

    Actualizacion fianl

--- src/App.js
+++ src/App.js
@@ -3,8 +3,33 @@ import Testimonio from "./componentes/Testimonio";
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div  className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestro alumnos sobre freeCodeCamp:</h1>
+      <Testimonio
+      nombre = 'Shawn Wang'
+      pais= 'Singapur'
+      imagen= 'shawn'
+      cargo= 'Ingeniero de Software'
+      empresa = 'Amazon'
+      testimonio= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'/>
+      <Testimonio
+      nombre = 'Sara Chima'
+      pais= 'Nigeria'
+      imagen= 'sarah'
+      cargo= 'Ingeniera de Software en ChatDesk'
+      empresa = 'ChatDesk'
+      testimonio= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'/>
+      <Testimonio
+      nombre = 'Emma Bostian'
+      pais= 'Suecia'
+      imagen= 'emma'
+      cargo= 'Ingeniera de Software'
+      empresa = 'Spotify'
+      testimonio= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'/>
+
+
     </div>
+  </div>
   );
 }
 
